3bc43f8915605497f6b302f47b42b11f5724a1b2
Rafael Silva <66369831+ralfsniper0102@users.noreply.github.com>
2025-02-09 19:36:13 -0300

    finish development

---
+++ Detect-Capital/Detect-Capital/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+public class Solution
+{
+    public bool DetectCapitalUse(string word)
+    {
+        return word == word.ToUpper() ||
+               word == word.ToLower() ||
+               (char.IsUpper(word[0]) && word.Substring(1) == word.Substring(1).ToLower());
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Solution solution = new Solution();
+
+        Console.WriteLine(solution.DetectCapitalUse("USA"));
+        Console.WriteLine(solution.DetectCapitalUse("FlaG"));
+        Console.WriteLine(solution.DetectCapitalUse("Google"));
+        Console.WriteLine(solution.DetectCapitalUse("leetcode"));
+        Console.WriteLine(solution.DetectCapitalUse("LeetCode"));
+    }
+}
